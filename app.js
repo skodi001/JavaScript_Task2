2961389a836595847759513ebb1ee89f98a8d4b7
Shokhmurod <shokhpjwstk@gmail.com>
2022-03-22 16:54:27 +0100

    practice of functions

--- app.js
+++ app.js
@@ -201,3 +201,20 @@ isInNineties(1987); //false
 const isNiceWeather = makeBetweenFunc(60, 79);
 isNiceWeather(68); //true
 isNiceWeather(98); //false
+
+function grumpus() {
+  alert("GAHHH GO AWAY!");
+}
+
+// setTimeout(callback, delay)
+setTimeout(function () {
+  //we pass an anonymous callback function
+  alert("WELCOME!");
+}, 5000);
+
+//DON'T WORRY ABOUT ANY OF THIS SYNTAX!!
+const btn = document.querySelector("button");
+// JUST FOCUS ON THE CALLBACK WE PASS IN!
+btn.addEventListener("click", function () {
+  alert("WHY DID YOU CLICK ME!!??");
+});
