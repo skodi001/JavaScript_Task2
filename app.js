fd01a547f819d58fd19ca91ad7a8d1e058bc636e
Shokhmurod <shokhpjwstk@gmail.com>
2022-03-22 12:01:04 +0100

    some random tasks about functions

--- app.js
+++ app.js
@@ -1,45 +1,45 @@
-//These variables are SCOPED to the function
-function lol() {
-  let person = "Tom";
-  const age = 45;
-  var color = "teal";
-  console.log(age);
-}
-// These variables are SCOPED to changeColor()
-function changeColor() {
-  let color = "purple";
-  const age = 19;
-  console.log(age);
-}
-lol();
-changeColor();
-age; //DOES NOT EXIST!
-color; //DOES NOT EXIST!
-person; //DOES NOT EXIST!
+// //These variables are SCOPED to the function
+// function lol() {
+//   let person = "Tom";
+//   const age = 45;
+//   var color = "teal";
+//   console.log(age);
+// }
+// // These variables are SCOPED to changeColor()
+// function changeColor() {
+//   let color = "purple";
+//   const age = 19;
+//   console.log(age);
+// }
+// lol();
+// changeColor();
+// age; //DOES NOT EXIST!
+// color; //DOES NOT EXIST!
+// person; //DOES NOT EXIST!
 
-let bird = "mandarin duck";
+// let bird = "mandarin duck";
 
-function birdWatch() {
-  //this bird is scoped to birdWatch()
-  let bird = "golden pheasant";
-  console.log(bird); //"golden pheasant"
-}
-birdWatch();
-console.log(bird); //"mandarin duck"
+// function birdWatch() {
+//   //this bird is scoped to birdWatch()
+//   let bird = "golden pheasant";
+//   console.log(bird); //"golden pheasant"
+// }
+// birdWatch();
+// console.log(bird); //"mandarin duck"
 
 // let & const are BLOCK SCOPED
-if (true) {
-  const animal = "eel";
-  console.log(animal); //'eel'
-}
-console.log(animal); //NOT DEFINED!
+// if (true) {
+//   const animal = "eel";
+//   console.log(animal); //'eel'
+// }
+// console.log(animal); //NOT DEFINED!
 
-// VAR IS NOT BLOCK SCOPED
-if (true) {
-  var animal = "eel";
-  console.log(animal); //'eel'
-}
-console.log(animal); //'eel'
+// // VAR IS NOT BLOCK SCOPED
+// if (true) {
+//   var animal = "eel";
+//   console.log(animal); //'eel'
+// }
+// console.log(animal); //'eel'
 
 // let animals = ['grizzly bear', 'panda bear', 'spectacled bear'];
 // var i = 10;
@@ -55,11 +55,42 @@ console.log(animal); //'eel'
 // }
 // console.log(i)
 
-function doubleArr(arr) {
-  const result = []; //scoped to the doubleArr function
-  for (let num of arr) {
-    let double = num * 2; //scoped to this loop
-    result.push(double);
+// function doubleArr(arr) {
+//   const result = []; //scoped to the doubleArr function
+//   for (let num of arr) {
+//     let double = num * 2; //scoped to this loop
+//     result.push(double);
+//   }
+//   return result;
+// }
+function outer() {
+  let movie = "Amadeus";
+
+  function inner() {
+    // let movie = "The Shining";
+
+    function extraInner() {
+      //movie is not defined in this function
+      //but it has access to parent function's variables
+      console.log(movie.toUpperCase());
+    }
+    extraInner();
   }
-  return result;
+  inner();
 }
+
+outer(); //'AMADEUS'
+// Function Statement
+function add(x, y) {
+  return x + y;
+}
+
+// Function Expression (Anonymous)
+const sum = function (x, y) {
+  return x + y;
+};
+
+// Function Expression (Named)
+const product = function multiply(x, y) {
+  return x * y;
+};
