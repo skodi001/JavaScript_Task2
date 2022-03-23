39c8c78b81e25d375eb8d3c192300a41d77a7765
Shokhmurod <shokhpjwstk@gmail.com>
2022-03-23 11:14:28 +0100

    practising forEach with different ways

--- app.js
+++ app.js
@@ -162,82 +162,138 @@
 //   }
 // }
 
-// This function returns a function!
-function multiplyBy(num) {
-  return function (x) {
-    return x * num;
-  };
-}
-
-//triple holds a function:
-const triple = multiplyBy(3);
-//we can call it:
-triple(4); //12
-triple(10); //30
-
-const double = multiplyBy(2);
-double(3); //6
-double(9); //18
-
-const halve = multiplyBy(0.5);
-halve(5); //2.5
-halve(100); //50
-
-// This function also acts as a "function factory"
-function makeBetweenFunc(x, y) {
-  return function (num) {
-    return num >= x && num <= y;
-  };
-}
-// This function checks if a value is between 0 and 18
-const isChild = makeBetweenFunc(0, 18);
-isChild(10); //true
-isChild(56); //false
-
-const isInNineties = makeBetweenFunc(1990, 1999);
-isInNineties(1994); //true
-isInNineties(1987); //false
-
-const isNiceWeather = makeBetweenFunc(60, 79);
-isNiceWeather(68); //true
-isNiceWeather(98); //false
-
-function grumpus() {
-  alert("GAHHH GO AWAY!");
-}
-
-// setTimeout(callback, delay)
-setTimeout(function () {
-  //we pass an anonymous callback function
-  alert("WELCOME!");
-}, 5000);
-
-//DON'T WORRY ABOUT ANY OF THIS SYNTAX!!
-const btn = document.querySelector("button");
-// JUST FOCUS ON THE CALLBACK WE PASS IN!
-btn.addEventListener("click", function () {
-  alert("WHY DID YOU CLICK ME!!??");
-});
-
-// variables declared with var are hoisted
-// console.log(animal);
-// var animal = 'Tapir';
-// console.log(animal);
-
-// variables declared with let & const are not hoisted
-// const shrimp = 'Harlequin Shrimp';
-// console.log(shrimp);
-
-// function statements are hoisted
-// howl();
-
-// function howl() {
-//   console.log("AWOOOOOOO");
+// // This function returns a function!
+// function multiplyBy(num) {
+//   return function (x) {
+//     return x * num;
+//   };
 // }
 
-// function expressions are...kind of hoisted.
-// The variable is hoisted, but has a value of undefined
-hoot();
-var hoot = function () {
-  console.log("HOOOO HOOOOO");
-};
+// //triple holds a function:
+// const triple = multiplyBy(3);
+// //we can call it:
+// triple(4); //12
+// triple(10); //30
+
+// const double = multiplyBy(2);
+// double(3); //6
+// double(9); //18
+
+// const halve = multiplyBy(0.5);
+// halve(5); //2.5
+// halve(100); //50
+
+// // This function also acts as a "function factory"
+// function makeBetweenFunc(x, y) {
+//   return function (num) {
+//     return num >= x && num <= y;
+//   };
+// }
+// // This function checks if a value is between 0 and 18
+// const isChild = makeBetweenFunc(0, 18);
+// isChild(10); //true
+// isChild(56); //false
+
+// const isInNineties = makeBetweenFunc(1990, 1999);
+// isInNineties(1994); //true
+// isInNineties(1987); //false
+
+// const isNiceWeather = makeBetweenFunc(60, 79);
+// isNiceWeather(68); //true
+// isNiceWeather(98); //false
+
+// function grumpus() {
+//   alert("GAHHH GO AWAY!");
+// }
+
+// // setTimeout(callback, delay)
+// setTimeout(function () {
+//   //we pass an anonymous callback function
+//   alert("WELCOME!");
+// }, 5000);
+
+// //DON'T WORRY ABOUT ANY OF THIS SYNTAX!!
+// const btn = document.querySelector("button");
+// // JUST FOCUS ON THE CALLBACK WE PASS IN!
+// btn.addEventListener("click", function () {
+//   alert("WHY DID YOU CLICK ME!!??");
+// });
+
+// // variables declared with var are hoisted
+// // console.log(animal);
+// // var animal = 'Tapir';
+// // console.log(animal);
+
+// // variables declared with let & const are not hoisted
+// // const shrimp = 'Harlequin Shrimp';
+// // console.log(shrimp);
+
+// // function statements are hoisted
+// // howl();
+
+// // function howl() {
+// //   console.log("AWOOOOOOO");
+// // }
+
+// // function expressions are...kind of hoisted.
+// // The variable is hoisted, but has a value of undefined
+// hoot();
+// var hoot = function () {
+//   console.log("HOOOO HOOOOO");
+// };
+const numbers = [20, 21, 22, 23, 24, 25, 26, 27];
+
+// Using anonymous function expression:
+numbers.forEach(function (num) {
+  console.log(num * 2);
+});
+
+function printTriple(n) {
+  console.log(n * 3);
+}
+
+// Using a pre-defined function:
+numbers.forEach(printTriple);
+
+// Using the index:
+numbers.forEach(function (num, idx) {
+  console.log(idx, num);
+});
+
+const books = [
+  {
+    title: "Good Omens",
+    authors: ["Terry Pratchett", "Neil Gaiman"],
+    rating: 4.25,
+  },
+  {
+    title: "Bone: The Complete Edition",
+    authors: ["Jeff Smith"],
+    rating: 4.42,
+  },
+  {
+    title: "American Gods",
+    authors: ["Neil Gaiman"],
+    rating: 4.11,
+  },
+  {
+    title: "A Gentleman in Moscow",
+    authors: ["Amor Towles"],
+    rating: 4.36,
+  },
+];
+
+// Printing all book titles using forEach:
+books.forEach(function (book) {
+  console.log(book.title.toUpperCase());
+});
+
+// Printing all book titles using for...of:
+for (let book of books) {
+  console.log(book.title.toUpperCase());
+}
+
+// Printing all book titles using for loop:
+for (let i = 0; i < books.length; i++) {
+  console.log(books[i].title.toUpperCase());
+}
