7182e6b7d63160f4dcafbe224722274534db7449
Shokhmurod <shokhpjwstk@gmail.com>
2022-03-23 11:31:13 +0100

    practising with map

--- app.js
+++ app.js
@@ -241,24 +241,83 @@
 // var hoot = function () {
 //   console.log("HOOOO HOOOOO");
 // };
+// const numbers = [20, 21, 22, 23, 24, 25, 26, 27];
+
+// // Using anonymous function expression:
+// numbers.forEach(function (num) {
+//   console.log(num * 2);
+// });
+
+// function printTriple(n) {
+//   console.log(n * 3);
+// }
+
+// // Using a pre-defined function:
+// numbers.forEach(printTriple);
+
+// // Using the index:
+// numbers.forEach(function (num, idx) {
+//   console.log(idx, num);
+// });
+
+// const books = [
+//   {
+//     title: "Good Omens",
+//     authors: ["Terry Pratchett", "Neil Gaiman"],
+//     rating: 4.25,
+//   },
+//   {
+//     title: "Bone: The Complete Edition",
+//     authors: ["Jeff Smith"],
+//     rating: 4.42,
+//   },
+//   {
+//     title: "American Gods",
+//     authors: ["Neil Gaiman"],
+//     rating: 4.11,
+//   },
+//   {
+//     title: "A Gentleman in Moscow",
+//     authors: ["Amor Towles"],
+//     rating: 4.36,
+//   },
+// ];
+
+// // Printing all book titles using forEach:
+// books.forEach(function (book) {
+//   console.log(book.title.toUpperCase());
+// });
+
+// // Printing all book titles using for...of:
+// for (let book of books) {
+//   console.log(book.title.toUpperCase());
+// }
+
+// // Printing all book titles using for loop:
+// for (let i = 0; i < books.length; i++) {
+//   console.log(books[i].title.toUpperCase());
+// }
+
 const numbers = [20, 21, 22, 23, 24, 25, 26, 27];
+const words = ["asap", "byob", "rsvp", "diy"];
 
-// Using anonymous function expression:
-numbers.forEach(function (num) {
-  console.log(num * 2);
+//Map creates a new array by calling your callback function with each element in the original array.
+const doubles = numbers.map(function (num) {
+  return num * 2; //Need to return the value!
+});
+//[40, 42, 44, 46, 48, 50, 52, 54]
+
+const numDetail = numbers.map(function (n) {
+  return {
+    value: n,
+    isEven: n % 2 === 0,
+  };
 });
 
-function printTriple(n) {
-  console.log(n * 3);
-}
-
-// Using a pre-defined function:
-numbers.forEach(printTriple);
-
-// Using the index:
-numbers.forEach(function (num, idx) {
-  console.log(idx, num);
+const abbrevs = words.map(function (word) {
+  return word.toUpperCase().split("").join(".");
 });
+//["A.S.A.P", "B.Y.O.B", "R.S.V.P", "D.I.Y"]
 
 const books = [
   {
@@ -283,17 +342,7 @@ const books = [
   },
 ];
 
-// Printing all book titles using forEach:
-books.forEach(function (book) {
-  console.log(book.title.toUpperCase());
+const titles = books.map(function (b) {
+  return b.title;
 });
-
-// Printing all book titles using for...of:
-for (let book of books) {
-  console.log(book.title.toUpperCase());
-}
-
-// Printing all book titles using for loop:
-for (let i = 0; i < books.length; i++) {
-  console.log(books[i].title.toUpperCase());
-}
+//["Good Omens", "Bone: The Complete Edition", "American Gods", "A Gentleman in Moscow"]
