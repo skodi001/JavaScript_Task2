ef4457eb6b7eeae4b96260a8d28b7cb703f7b159
Shokhmurod <shokhpjwstk@gmail.com>
2022-03-23 11:57:02 +0100

    practising with arrow functions

--- app.js
+++ app.js
@@ -298,51 +298,111 @@
 //   console.log(books[i].title.toUpperCase());
 // }
 
-const numbers = [20, 21, 22, 23, 24, 25, 26, 27];
-const words = ["asap", "byob", "rsvp", "diy"];
+// const numbers = [20, 21, 22, 23, 24, 25, 26, 27];
+// const words = ["asap", "byob", "rsvp", "diy"];
 
-//Map creates a new array by calling your callback function with each element in the original array.
-const doubles = numbers.map(function (num) {
-  return num * 2; //Need to return the value!
+// //Map creates a new array by calling your callback function with each element in the original array.
+// const doubles = numbers.map(function (num) {
+//   return num * 2; //Need to return the value!
+// });
+// //[40, 42, 44, 46, 48, 50, 52, 54]
+
+// const numDetail = numbers.map(function (n) {
+//   return {
+//     value: n,
+//     isEven: n % 2 === 0,
+//   };
+// });
+
+// const abbrevs = words.map(function (word) {
+//   return word.toUpperCase().split("").join(".");
+// });
+// //["A.S.A.P", "B.Y.O.B", "R.S.V.P", "D.I.Y"]
+
+// const books = [
+//   {
+//     title: "Good Omens",
+//     authors: ["Terry Pratchett", "Neil Gaiman"],
+//     rating: 4.25,
+//   },
+//   {
+//     title: "Bone: The Complete Edition",
+//     authors: ["Jeff Smith"],
+//     rating: 4.42,
+//   },
+//   {
+//     title: "American Gods",
+//     authors: ["Neil Gaiman"],
+//     rating: 4.11,
+//   },
+//   {
+//     title: "A Gentleman in Moscow",
+//     authors: ["Amor Towles"],
+//     rating: 4.36,
+//   },
+// ];
+
+// const titles = books.map(function (b) {
+//   return b.title;
+// });
+// //["Good Omens", "Bone: The Complete Edition", "American Gods", "A Gentleman in Moscow"]
+//Non-arrow function expression:
+// const square = function (x) {
+//   return x * x;
+// }
+
+// Equivalent arrow function:
+const square = (x) => {
+  return x * x;
+};
+
+//parens are option when you have 1 parameter
+const isEven = (num) => {
+  return num % 2 === 0;
+};
+
+//Must include parens for multiple parameters
+const multiply = (x, y) => {
+  return x * y;
+};
+
+// Must include parens for zero parameters
+const greet = () => {
+  console.log("HI!");
+};
+
+// "Regular" arrow function:
+const square = (n) => {
+  return n * n;
+};
+// Implicit Return, on multiple lines using parens
+const square1 = (n) => n * n;
+
+// Implicit return one-liner:
+const square2 = (n) => n * n;
+
+const nums = [1, 2, 3, 4, 5, 6, 7, 8];
+
+// ALL THREE VERSIONS GIVE US THE SAME RESULT:
+const doubles1 = nums.map(function (n) {
+  return n * 2;
 });
-//[40, 42, 44, 46, 48, 50, 52, 54]
 
-const numDetail = numbers.map(function (n) {
-  return {
-    value: n,
-    isEven: n % 2 === 0,
-  };
+const doubles2 = nums.map((n) => {
+  return n * 2;
 });
 
-const abbrevs = words.map(function (word) {
-  return word.toUpperCase().split("").join(".");
-});
-//["A.S.A.P", "B.Y.O.B", "R.S.V.P", "D.I.Y"]
+const doubles3 = nums.map((n) => n * 2);
 
-const books = [
-  {
-    title: "Good Omens",
-    authors: ["Terry Pratchett", "Neil Gaiman"],
-    rating: 4.25,
-  },
-  {
-    title: "Bone: The Complete Edition",
-    authors: ["Jeff Smith"],
-    rating: 4.42,
-  },
-  {
-    title: "American Gods",
-    authors: ["Neil Gaiman"],
-    rating: 4.11,
-  },
-  {
-    title: "A Gentleman in Moscow",
-    authors: ["Amor Towles"],
-    rating: 4.36,
-  },
-];
-
-const titles = books.map(function (b) {
-  return b.title;
+const parityList = nums.map(function (n) {
+  if (n % 2 === 0) return "even";
+  return "odd";
 });
-//["Good Omens", "Bone: The Complete Edition", "American Gods", "A Gentleman in Moscow"]
+
+const parityList1 = nums.map((n) => {
+  if (n % 2 === 0) return "even";
+  return "odd";
+});
+const parityList2 = nums.map((n) => (n % 2 === 0 ? "even" : "odd"));
+
+const parityList3 = nums.map((n) => (n % 2 === 0 ? "even" : "odd"));
