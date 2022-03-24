a6721209724b86647e62c0c6057aafc961d9c0d7
Shokhmurod <shokhpjwstk@gmail.com>
2022-03-24 16:53:59 +0100

    trying to understand reduce better with some practices

--- app.js
+++ app.js
@@ -532,97 +532,218 @@
 //   return title.includes(query.toLowerCase());
 // });
 
-const words = ["dog", "dig", "log", "bag", "wag"];
+// const words = ["dog", "dig", "log", "bag", "wag"];
 
-//Is every word 3 characters long?
-const all3Letters = words.every((word) => word.length === 3);
+// //Is every word 3 characters long?
+// const all3Letters = words.every((word) => word.length === 3);
 
-// Do all words end in 'g'?
-const allEndInG = words.every((word) => {
-  const last = word.length - 1;
-  return word[last] === "g";
+// // Do all words end in 'g'?
+// const allEndInG = words.every((word) => {
+//   const last = word.length - 1;
+//   return word[last] === "g";
+// });
+
+// //Does at least 1 book start with 'd'?
+// const someStartWithD = words.some((word) => word[0] === "d");
+
+// // Do all words start with 'd'?
+// const allStartWithD = words.every((word) => word[0] === "d");
+
+// const books = [
+//   {
+//     title: "Good Omens",
+//     authors: ["Terry Pratchett", "Neil Gaiman"],
+//     rating: 4.25,
+//     genres: ["fiction", "fantasy"],
+//   },
+//   {
+//     title: "Changing My Mind",
+//     authors: ["Zadie Smith"],
+//     rating: 3.83,
+//     genres: ["nonfiction", "essays"],
+//   },
+//   {
+//     title: "Bone: The Complete Edition",
+//     authors: ["Jeff Smith"],
+//     rating: 4.42,
+//     genres: ["fiction", "graphic novel", "fantasy"],
+//   },
+//   {
+//     title: "American Gods",
+//     authors: ["Neil Gaiman"],
+//     rating: 4.11,
+//     genres: ["fiction", "fantasy"],
+//   },
+//   {
+//     title: "A Gentleman in Moscow",
+//     authors: ["Amor Towles"],
+//     rating: 4.36,
+//     genres: ["fiction", "historical fiction"],
+//   },
+//   {
+//     title: "The Name of the Wind",
+//     authors: ["Patrick Rothfuss"],
+//     rating: 4.54,
+//     genres: ["fiction", "fantasy"],
+//   },
+//   {
+//     title: "The Overstory",
+//     authors: ["Richard Powers"],
+//     rating: 4.19,
+//     genres: ["fiction", "short stories"],
+//   },
+//   {
+//     title: "The Way of Kings",
+//     authors: ["Brandon Sanderson"],
+//     rating: 4.65,
+//     genres: ["fantasy", "epic"],
+//   },
+//   {
+//     title: "Lord of the flies",
+//     authors: ["William Golding"],
+//     rating: 3.67,
+//     genres: ["fiction"],
+//   },
+// ];
+// //Are all books rated 3.5 or higher?
+// const allGoodBooks = books.every((book) => book.rating > 3.5);
+
+// //Do any books have 2 authors?
+// const any2Authors = books.some((book) => book.authors.length === 2);
+
+// const prices = [400.5, 3000, 99.99, 35.99, 12.0, 9500];
+// // REMEMBER THAT SORT() MUTATES THE ORIGINAL ARRAY!
+// // I'm using slice() to create a new copy, otherwise we would be sorting the same array 3 times:
+
+// // Default string sort :(
+// const badSort = prices.slice().sort();
+
+// // Ascending Sort:
+// const ascSort = prices.slice().sort((a, b) => a - b);
+
+// // Descending Sort:
+// const descSort = prices.slice().sort((a, b) => b - a);
+
+// const books = [
+//   {
+//     title: "Good Omens",
+//     authors: ["Terry Pratchett", "Neil Gaiman"],
+//     rating: 4.25,
+//     genres: ["fiction", "fantasy"],
+//   },
+//   {
+//     title: "Changing My Mind",
+//     authors: ["Zadie Smith"],
+//     rating: 3.83,
+//     genres: ["nonfiction", "essays"],
+//   },
+//   {
+//     title: "Bone: The Complete Edition",
+//     authors: ["Jeff Smith"],
+//     rating: 4.42,
+//     genres: ["fiction", "graphic novel", "fantasy"],
+//   },
+//   {
+//     title: "American Gods",
+//     authors: ["Neil Gaiman"],
+//     rating: 4.11,
+//     genres: ["fiction", "fantasy"],
+//   },
+//   {
+//     title: "A Gentleman in Moscow",
+//     authors: ["Amor Towles"],
+//     rating: 4.36,
+//     genres: ["fiction", "historical fiction"],
+//   },
+//   {
+//     title: "The Name of the Wind",
+//     authors: ["Patrick Rothfuss"],
+//     rating: 4.54,
+//     genres: ["fiction", "fantasy"],
+//   },
+//   {
+//     title: "The Overstory",
+//     authors: ["Richard Powers"],
+//     rating: 4.19,
+//     genres: ["fiction", "short stories"],
+//   },
+//   {
+//     title: "The Way of Kings",
+//     authors: ["Brandon Sanderson"],
+//     rating: 4.65,
+//     genres: ["fantasy", "epic"],
+//   },
+//   {
+//     title: "Lord of the flies",
+//     authors: ["William Golding"],
+//     rating: 3.67,
+//     genres: ["fiction"],
+//   },
+// ];
+
+// // Sorting books by their rating:
+// books.sort((a, b) => b.rating - a.rating);
+
+const nums = [3, 4, 5, 6, 7];
+// To multiply all values in nums:
+const product = nums.reduce((total, currentVal) => {
+  return total * currentVal;
 });
 
-//Does at least 1 book start with 'd'?
-const someStartWithD = words.some((word) => word[0] === "d");
+// total    currentVal    returnVal
+// 3             4           12
+// 12            5           60
+// 60            6           360
+// 360           7           2520
 
-// Do all words start with 'd'?
-const allStartWithD = words.every((word) => word[0] === "d");
+//Final Return Value: 2520
+const grades = [87, 64, 96, 92, 88, 99, 73, 70, 64];
 
-const books = [
-  {
-    title: "Good Omens",
-    authors: ["Terry Pratchett", "Neil Gaiman"],
-    rating: 4.25,
-    genres: ["fiction", "fantasy"],
-  },
-  {
-    title: "Changing My Mind",
-    authors: ["Zadie Smith"],
-    rating: 3.83,
-    genres: ["nonfiction", "essays"],
-  },
-  {
-    title: "Bone: The Complete Edition",
-    authors: ["Jeff Smith"],
-    rating: 4.42,
-    genres: ["fiction", "graphic novel", "fantasy"],
-  },
-  {
-    title: "American Gods",
-    authors: ["Neil Gaiman"],
-    rating: 4.11,
-    genres: ["fiction", "fantasy"],
-  },
-  {
-    title: "A Gentleman in Moscow",
-    authors: ["Amor Towles"],
-    rating: 4.36,
-    genres: ["fiction", "historical fiction"],
-  },
-  {
-    title: "The Name of the Wind",
-    authors: ["Patrick Rothfuss"],
-    rating: 4.54,
-    genres: ["fiction", "fantasy"],
-  },
-  {
-    title: "The Overstory",
-    authors: ["Richard Powers"],
-    rating: 4.19,
-    genres: ["fiction", "short stories"],
-  },
-  {
-    title: "The Way of Kings",
-    authors: ["Brandon Sanderson"],
-    rating: 4.65,
-    genres: ["fantasy", "epic"],
-  },
-  {
-    title: "Lord of the flies",
-    authors: ["William Golding"],
-    rating: 3.67,
-    genres: ["fiction"],
-  },
-];
-//Are all books rated 3.5 or higher?
-const allGoodBooks = books.every((book) => book.rating > 3.5);
+// One approach to find max using reduce:
+// const maxGrade = grades.reduce((max, currVal) => {
+//   if (currVal > max) return currVal;
+//   return max;
+// });
 
-//Do any books have 2 authors?
-const any2Authors = books.some((book) => book.authors.length === 2);
+// A shorter version using Math.max:
+const maxGrade = grades.reduce((max, currVal) => {
+  return Math.max(max, currVal);
+});
+// Finding the min value using Math.min:
+const minGrade = grades.reduce((min, currVal) =>
+  //using implicit return for variety's sake
+  Math.min(min, currVal)
+);
 
-const prices = [400.5, 3000, 99.99, 35.99, 12.0, 9500];
-// REMEMBER THAT SORT() MUTATES THE ORIGINAL ARRAY!
-// I'm using slice() to create a new copy, otherwise we would be sorting the same array 3 times:
+// max     currVal    return
+// 87        64         87
+// 87        96         96
+// 96        92         96
+/// etc.
 
-// Default string sort :(
-const badSort = prices.slice().sort();
+// We can specify an initial value as the 2nd argument to reduce:
+// arr.reduce(reducerFunction, initialValue)
+const total = [10, 20, 30, 40, 50].reduce((sum, currVal) => {
+  return sum + currVal;
+}, 1000); //sum starts at 1000, then each element is added to it
 
-// Ascending Sort:
-const ascSort = prices.slice().sort((a, b) => a - b);
+const votes = ["y", "y", "n", "y", "n", "y", "n", "y", "n", "n", "n", "y", "y"];
 
-// Descending Sort:
-const descSort = prices.slice().sort((a, b) => b - a);
+// To tally the votes:
+// const results = votes.reduce((tally, val) => {
+//   if (tally[val]) {
+//     tally[val]++
+//   } else {
+//     tally[val] = 1;
+//   }
+//   return tally;
+// }, {})
+
+// The shorter version:
+const results = votes.reduce((tally, val) => {
+  tally[val] = (tally[val] || 0) + 1;
+  return tally;
+}, {});
 
 const books = [
   {
@@ -668,6 +789,12 @@ const books = [
     genres: ["fiction", "short stories"],
   },
   {
+    title: "A Truly Horrible Book",
+    authors: ["Xavier Time"],
+    rating: 2.18,
+    genres: ["fiction", "garbage"],
+  },
+  {
     title: "The Way of Kings",
     authors: ["Brandon Sanderson"],
     rating: 4.65,
@@ -680,6 +807,10 @@ const books = [
     genres: ["fiction"],
   },
 ];
-
-// Sorting books by their rating:
-books.sort((a, b) => b.rating - a.rating);
+// To group books by rating:
+const groupedByRatings = books.reduce((groupedBooks, book) => {
+  const key = Math.floor(book.rating);
+  if (!groupedBooks[key]) groupedBooks[key] = [];
+  groupedBooks[key].push(book);
+  return groupedBooks;
+}, {});
