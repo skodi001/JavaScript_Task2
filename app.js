6fef1c7b55483053a3c95978f06e0d6b9c1cf15e
Shokhmurod <shokhpjwstk@gmail.com>
2022-03-26 09:42:43 +0100

    practicing with rest parameters

--- app.js
+++ app.js
@@ -951,15 +951,40 @@ const votes = ["y", "y", "n", "y", "n", "y", "n", "y", "n", "n", "n", "y", "y"];
 
 //The arguments object is available in every function you write (except arrow functions)
 //It contains all the arguments passed in.
-function sum() {
-  //It is NOT an array, we have to turn it into one if we want to use array methods
-  const argsArr = [...arguments];
-  return argsArr.reduce((total, currVal) => {
+// function sum() {
+//   //It is NOT an array, we have to turn it into one if we want to use array methods
+//   const argsArr = [...arguments];
+//   return argsArr.reduce((total, currVal) => {
+//     return total + currVal;
+//   });
+// }
+
+// // No arguments object inside of arrow functions :(
+// const multiply = () => {
+//   console.log(arguments);
+// };
+
+// OLD WAY!
+// function sum() {
+//   const argsArr = [...arguments]
+//   return argsArr.reduce((total, currVal) => {
+//     return total + currVal
+//   })
+// }
+
+// New way using rest:
+function sum(...nums) {
+  return nums.reduce((total, currVal) => {
     return total + currVal;
   });
 }
 
-// No arguments object inside of arrow functions :(
-const multiply = () => {
-  console.log(arguments);
-};
+//We can have named params and then collect the rest into an array:
+function fullName(first, last, ...titles) {
+  console.log("first", first);
+  console.log("last", last);
+  console.log("titles", titles);
+}
+
+// We can use rest parameters in arrow functions!
+const multiply = (...nums) => nums.reduce((total, currVal) => total * currVal);
