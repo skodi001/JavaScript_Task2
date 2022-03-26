bfc242778110c919f063efe115171e02f7f46764
Shokhmurod <shokhpjwstk@gmail.com>
2022-03-26 17:46:08 +0100

    practice with destructing objects

--- app.js
+++ app.js
@@ -1016,3 +1016,19 @@ fourth; //'Ghirmay Ghebreslassie'
 const [winner, ...others] = raceResults;
 winner; //'Eliud Kipchoge'
 others; //["Feyisa Lelisa", "Galen Rupp", "Ghirmay Ghebreslassie", "Alphonce Simbu", "Jared Ward"]
+const runner = {
+  first: "Eliud",
+  last: "Kipchoge",
+  country: "Kenya",
+  title: "Elder of the Order of the Golden Heart of Kenya",
+};
+
+// const {
+//   first,
+//   last,
+//   time
+// } = runner;
+
+const { country: nation, title: honorific } = runner;
+
+const { first, last, ...other } = runner;
