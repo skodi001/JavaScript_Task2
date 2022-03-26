50536617e49916edf53b486efe72754d81770ca9
Shokhmurod <shokhpjwstk@gmail.com>
2022-03-26 10:32:07 +0100

    some random examples

--- app.js
+++ app.js
@@ -988,3 +988,31 @@ function fullName(first, last, ...titles) {
 
 // We can use rest parameters in arrow functions!
 const multiply = (...nums) => nums.reduce((total, currVal) => total * currVal);
+
+const raceResults = [
+  "Eliud Kipchoge",
+  "Feyisa Lelisa",
+  "Galen Rupp",
+  "Ghirmay Ghebreslassie",
+  "Alphonce Simbu",
+  "Jared Ward",
+];
+
+// The old way:
+// const gold = raceResults[0]
+// const silver = raceResults[1]
+// const bronze = raceResults[2]
+
+// Using Destructuring:
+const [gold, silver, bronze] = raceResults;
+gold; //'Eliud Kipchoge'
+silver; //'Feyisa Lelisa'
+bronze; //'Galen Rupp'
+
+const [first, , , fourth] = raceResults;
+first; //'Eliud Kipchoge'
+fourth; //'Ghirmay Ghebreslassie'
+
+const [winner, ...others] = raceResults;
+winner; //'Eliud Kipchoge'
+others; //["Feyisa Lelisa", "Galen Rupp", "Ghirmay Ghebreslassie", "Alphonce Simbu", "Jared Ward"]
