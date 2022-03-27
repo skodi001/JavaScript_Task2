889756aee5883dadad44e80b1d3e2ea728c72197
Shokhmurod <shokhpjwstk@gmail.com>
2022-03-27 18:50:57 +0200

    practicing to add method to objects

--- app.js
+++ app.js
@@ -1097,51 +1097,105 @@ const votes = ["y", "y", "n", "y", "n", "y", "n", "y", "n", "n", "n", "y", "y"];
 //   }
 // }
 
-const getStats = (arr) => {
-  const max = Math.max(...arr);
-  const min = Math.min(...arr);
-  const sum = arr.reduce((sum, r) => sum + r);
-  const avg = sum / arr.length;
-  // Using the new shorthand property syntax:
-  return {
-    max,
-    min,
-    sum,
-    avg,
-  };
+// const getStats = (arr) => {
+//   const max = Math.max(...arr);
+//   const min = Math.min(...arr);
+//   const sum = arr.reduce((sum, r) => sum + r);
+//   const avg = sum / arr.length;
+//   // Using the new shorthand property syntax:
+//   return {
+//     max,
+//     min,
+//     sum,
+//     avg,
+//   };
+// };
+// const reviews = [4.5, 5.0, 3.44, 2.8, 3.5, 4.0, 3.5];
+
+// const stats = getStats(reviews);
+
+// function pick(arr) {
+//   //return random element from arr
+//   const idx = Math.floor(Math.random() * arr.length);
+//   return arr[idx];
+// }
+
+// function getCard() {
+//   const values = [
+//     "1",
+//     "2",
+//     "3",
+//     "4",
+//     "5",
+//     "6",
+//     "7",
+//     "8",
+//     "9",
+//     "10",
+//     "J",
+//     "Q",
+//     "K",
+//     "A",
+//   ];
+//   const suits = ["clubs", "spades", "hearts", "diamonds"];
+//   const value = pick(values);
+//   const suit = pick(suits);
+//   return {
+//     value,
+//     suit,
+//   };
+// }
+
+const role = "host";
+const person = "Jools Holland";
+const role2 = "Director";
+const person2 = "James Cameron";
+
+// The old way:
+// Make the object
+// const team = {};
+// Then add a property using dynamic key:
+// team[role] = person;
+// team[role2] = person2;
+
+// USING COMPUTED PROPERTIES!
+const team = {
+  [role]: person,
+  [role2]: person2,
+  [1 + 6 + 9]: "sixteen",
 };
-const reviews = [4.5, 5.0, 3.44, 2.8, 3.5, 4.0, 3.5];
 
-const stats = getStats(reviews);
+// function addProp(obj, k, v) {
+//   const copy = {
+//     ...obj
+//   };
+//   copy[k] = v;
+//   return copy;
+// }
 
-function pick(arr) {
-  //return random element from arr
-  const idx = Math.floor(Math.random() * arr.length);
-  return arr[idx];
-}
+// const addProp = (obj, k, v) => {
+//   return {
+//     ...obj,
+//     [k]: v
+//   }
+// }
 
-function getCard() {
-  const values = [
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "J",
-    "Q",
-    "K",
-    "A",
-  ];
-  const suits = ["clubs", "spades", "hearts", "diamonds"];
-  const value = pick(values);
-  const suit = pick(suits);
-  return {
-    value,
-    suit,
-  };
-}
+const addProp = (obj, k, v) => ({
+  ...obj,
+  [k]: v,
+});
+const res = addProp(team, "happy", ":)");
+
+// Adding methods to an object!
+const math = {
+  numbers: [1, 2, 3, 4, 5],
+  add: function (x, y) {
+    return x + y;
+  },
+  multiply: function (x, y) {
+    return x * y;
+  },
+};
+
+// To execute multiply:
+math.multiply(5, 9); //45
