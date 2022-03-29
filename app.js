623f33eb59d32bea4164b257467fc8147e8e1470
Shokhmurod <shokhpjwstk@gmail.com>
2022-03-29 03:48:34 +0200

    creating deck and working in this

--- app.js
+++ app.js
@@ -1241,61 +1241,221 @@ const votes = ["y", "y", "n", "y", "n", "y", "n", "y", "n", "n", "n", "y", "y"];
 //   },
 // };
 
-function sayHi() {
-  console.log("HI");
-  //this refers to the window (global scope object in the browser)
-  console.log(this);
+// function sayHi() {
+//   console.log("HI");
+//   //this refers to the window (global scope object in the browser)
+//   console.log(this);
+// }
+
+// const person = {
+//   first: "Cherilyn",
+//   last: "Sarkisian",
+//   nickName: "Cher",
+//   fullName() {
+//     //In a method, this refers to the object the method "lives" in
+//     const { first, last, nickName } = this;
+//     return `${first} ${last} AKA ${nickName}`;
+//   },
+//   printBio() {
+//     console.log(this);
+//     const fullName = this.fullName();
+//     console.log(`${fullName} is a person!`);
+//   },
+//   laugh: () => {
+//     //Arrow functions don't get their 'own' this.
+//     console.log(this);
+//     console.log(`${this.nickName} says HAHAHAHAH`);
+//   },
+// };
+
+// // INVOCATION CONTEXT MATTERS!!!
+// person.printBio(); //THIS refers to the person object
+
+// const printBio = person.printBio;
+// printBio(); //THIS refers to window object
+
+// const annoyer = {
+//   phrases: [
+//     "literally",
+//     "cray cray",
+//     "I can't even",
+//     "Totes!",
+//     "YOLO",
+//     "Can't Stop, Won't Stop",
+//   ],
+//   pickPhrase() {
+//     const { phrases } = this;
+//     const idx = Math.floor(Math.random() * phrases.length);
+//     return phrases[idx];
+//   },
+//   start() {
+//     //Use an arrow function to avoid getting a different 'this':
+//     this.timerId = setInterval(() => {
+//       console.log(this.pickPhrase());
+//     }, 3000);
+//   },
+//   stop() {
+//     clearInterval(this.timerId);
+//     console.log("PHEW THANK HEAVENS THAT IS OVER!");
+//   },
+// };
+
+// **********************************
+// WRITING EVERYTHING USING FUNCTIONS
+// **********************************
+function initializeDeck() {
+  const deck = [];
+  const suits = ["hearts", "diamonds", "spades", "clubs"];
+  const values = "2,3,4,5,6,7,8,9,10,J,Q,K,A";
+  for (let value of values.split(",")) {
+    for (let suit of suits) {
+      deck.push({
+        value,
+        suit,
+      });
+    }
+  }
+  return deck;
 }
 
-const person = {
-  first: "Cherilyn",
-  last: "Sarkisian",
-  nickName: "Cher",
-  fullName() {
-    //In a method, this refers to the object the method "lives" in
-    const { first, last, nickName } = this;
-    return `${first} ${last} AKA ${nickName}`;
+function drawCard(deck, drawnCards) {
+  const card = deck.pop();
+  drawnCards.push(card);
+  return card;
+}
+
+function drawMultiple(numCards, deck, drawnCards) {
+  const cards = [];
+  for (let i = 0; i < numCards; i++) {
+    cards.push(drawCard(deck, drawnCards));
+  }
+  return cards;
+}
+
+function shuffle(deck) {
+  // loop over array backwards
+  for (let i = deck.length - 1; i > 0; i--) {
+    //pick random index before current element
+    let j = Math.floor(Math.random() * (i + 1));
+    //swap
+    [deck[i], deck[j]] = [deck[j], deck[i]];
+  }
+  return deck;
+}
+
+// We have to create a bunch of variables:
+const firstDeck = initializeDeck();
+const drawnCards = [];
+shuffle(firstDeck);
+// We have to pass a ton of arguments around:
+const hand1 = drawMultiple(2, firstDeck, drawnCards);
+const hand2 = drawMultiple(2, firstDeck, drawnCards);
+const pokerHand = drawMultiple(5, firstDeck, drawnCards);
+
+// **********************************
+// USING AN OBJECT + METHODS INSTEAD:
+// **********************************
+
+const myDeck = {
+  deck: [],
+  drawnCards: [],
+  suits: ["hearts", "diamonds", "spades", "clubs"],
+  values: "2,3,4,5,6,7,8,9,10,J,Q,K,A",
+  initializeDeck() {
+    const { suits, values, deck } = this;
+    for (let value of values.split(",")) {
+      for (let suit of suits) {
+        deck.push({
+          value,
+          suit,
+        });
+      }
+    }
+    // return deck;
   },
-  printBio() {
-    console.log(this);
-    const fullName = this.fullName();
-    console.log(`${fullName} is a person!`);
+  drawCard() {
+    const card = this.deck.pop();
+    this.drawnCards.push(card);
+    return card;
   },
-  laugh: () => {
-    //Arrow functions don't get their 'own' this.
-    console.log(this);
-    console.log(`${this.nickName} says HAHAHAHAH`);
+  drawMultiple(numCards) {
+    const cards = [];
+    for (let i = 0; i < numCards; i++) {
+      cards.push(this.drawCard());
+    }
+    return cards;
+  },
+  shuffle() {
+    const { deck } = this;
+    // loop over array backwards
+    for (let i = deck.length - 1; i > 0; i--) {
+      //pick random index before current element
+      let j = Math.floor(Math.random() * (i + 1));
+      //swap
+      [deck[i], deck[j]] = [deck[j], deck[i]];
+    }
   },
 };
 
-// INVOCATION CONTEXT MATTERS!!!
-person.printBio(); //THIS refers to the person object
+// Much cleaner!!
+myDeck.initializeDeck();
+myDeck.shuffle();
+const h1 = myDeck.drawMultiple(2);
+const h2 = myDeck.drawMultiple(2);
+const h3 = myDeck.drawMultiple(5);
 
-const printBio = person.printBio;
-printBio(); //THIS refers to window object
-
-const annoyer = {
-  phrases: [
-    "literally",
-    "cray cray",
-    "I can't even",
-    "Totes!",
-    "YOLO",
-    "Can't Stop, Won't Stop",
-  ],
-  pickPhrase() {
-    const { phrases } = this;
-    const idx = Math.floor(Math.random() * phrases.length);
-    return phrases[idx];
-  },
-  start() {
-    //Use an arrow function to avoid getting a different 'this':
-    this.timerId = setInterval(() => {
-      console.log(this.pickPhrase());
-    }, 3000);
-  },
-  stop() {
-    clearInterval(this.timerId);
-    console.log("PHEW THANK HEAVENS THAT IS OVER!");
-  },
+// THIS FUNCTION RETURNS A NEW DECK EVERYTIME WE CALL IT!
+const makeDeck = () => {
+  return {
+    deck: [],
+    drawnCards: [],
+    suits: ["hearts", "diamonds", "spades", "clubs"],
+    values: "2,3,4,5,6,7,8,9,10,J,Q,K,A",
+    initializeDeck() {
+      const { suits, values, deck } = this;
+      for (let value of values.split(",")) {
+        for (let suit of suits) {
+          deck.push({
+            value,
+            suit,
+          });
+        }
+      }
+      // return deck;
+    },
+    drawCard() {
+      const card = this.deck.pop();
+      this.drawnCards.push(card);
+      return card;
+    },
+    drawMultiple(numCards) {
+      const cards = [];
+      for (let i = 0; i < numCards; i++) {
+        cards.push(this.drawCard());
+      }
+      return cards;
+    },
+    shuffle() {
+      const { deck } = this;
+      // loop over array backwards
+      for (let i = deck.length - 1; i > 0; i--) {
+        //pick random index before current element
+        let j = Math.floor(Math.random() * (i + 1));
+        //swap
+        [deck[i], deck[j]] = [deck[j], deck[i]];
+      }
+    },
+  };
 };
+
+// OUR FIRST DECK!
+const myDeck = makeDeck();
+myDeck.initializeDeck();
+myDeck.shuffle();
+const h1 = myDeck.drawMultiple(2);
+const h2 = myDeck.drawMultiple(2);
+const h3 = myDeck.drawMultiple(5);
+
+// OUR SECOND DECK!
+const deck2 = makeDeck();
+deck2.initializeDeck();
