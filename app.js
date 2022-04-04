6124a3227cdf902c302244fc9ad0ce41218421ec
Shokhmurod <shokhpjwstk@gmail.com>
2022-04-04 08:35:52 +0200

    practice with addEventListener

--- app.js
+++ app.js
@@ -1654,3 +1654,25 @@ btn.onclick = () => {
 function greet() {
   alert("HEY BUDDY!");
 }
+
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", function () {
+  alert("CLICKED!!!");
+});
+
+btn.addEventListener("click", function () {
+  console.log("SECOND THING!!");
+});
+
+btn.addEventListener("mouseover", function () {
+  btn.innerText = "STOP TOUCHING ME";
+});
+
+btn.addEventListener("mouseout", function () {
+  btn.innerText = "Click Me!";
+});
+
+window.addEventListener("scroll", function () {
+  console.log("STOP SCROLLING!!");
+});
