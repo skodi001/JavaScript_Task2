adfef11de4cbd6130dffeed2237f0c21043428f4
Shokhmurod <shokhpjwstk@gmail.com>
2022-04-04 08:34:06 +0200

    exercises to add event, trying to understand inline options

--- app.js
+++ app.js
@@ -1461,167 +1461,196 @@
 // deck2.initializeDeck();
 
 // To select the element with id of bear-photo:
-document.getElementById("bear-photo");
+// document.getElementById("bear-photo");
 
-//To select the element with id of main:
-document.getElementById("main");
+// //To select the element with id of main:
+// document.getElementById("main");
 
-//To select all li's
-document.getElementsByTagName("li");
+// //To select all li's
+// document.getElementsByTagName("li");
 
-// To select all h1's (there's only one on this page):
-document.getElementsByTagName("h1");
+// // To select all h1's (there's only one on this page):
+// document.getElementsByTagName("h1");
 
-//Remember, getElementsByTagName returns an array-like object (NOT a real array)
-const inputs = document.getElementsByTagName("input"); //get all inputs
-inputs[0]; //this works
-inputs.pop(); //DOES NOT WORK! pop() is an array method, and this isn't an array!
+// //Remember, getElementsByTagName returns an array-like object (NOT a real array)
+// const inputs = document.getElementsByTagName("input"); //get all inputs
+// inputs[0]; //this works
+// inputs.pop(); //DOES NOT WORK! pop() is an array method, and this isn't an array!
 
-// To select all elements with the class of 'special':
-document.getElementsByClassName("special");
+// // To select all elements with the class of 'special':
+// document.getElementsByClassName("special");
 
-//getElementsByClassName also returns an array-like object.
-// We don't have access to array methods, but we can iterate:
+// //getElementsByClassName also returns an array-like object.
+// // We don't have access to array methods, but we can iterate:
 
-const specials = document.getElementsByClassName("special");
-for (let el of specials) {
-  console.log(el);
+// const specials = document.getElementsByClassName("special");
+// for (let el of specials) {
+//   console.log(el);
+// }
+
+// //We can use spread to make an actual array:
+// const arr = [...specials];
+// arr.pop(); //this works because it's now an array!
+
+// // ****************
+// // querySelector
+// // ****************
+// //To find the first li on the page:
+// document.querySelector("li");
+
+// //To find the first element with class of special:
+// document.querySelector(".special");
+
+// //To find the first element with id of main (there should only be one...)
+// document.querySelector("#main");
+
+// // To find the first li with the class of special, nested inside of a ul, nested inside a section:
+// document.querySelector("section ul li.special");
+
+// // ****************
+// // querySelectorAll
+// // ****************
+
+// // To find ALL li's on the page:
+// document.querySelectorAll("li");
+
+// // To find ALL elements with the class of special on the page:
+// document.querySelectorAll(".special");
+
+// //
+// // Select all LI's on the page:
+// const allLis = document.querySelectorAll("li");
+
+// // One option, a regular for loop:
+// for (let i = 0; i < allLis.length; i++) {
+//   allLis[i].innerText = "WE ARE THE CHAMPIONS!";
+// }
+
+// //Another option using for...of:
+// for (let li of allLis) {
+//   li.innerHTML = "WE ARE <b>THE CHAMPIONS</b>";
+// }
+// // Changing the color and background-color:
+// const h1 = document.querySelector("h1");
+// h1.style.color = "pink";
+// h1.style.backgroundColor = "yellow"; //camel cased! (not background-color but backgroundColor)
+
+// // Changing Multiple Elements:
+// const allLis = document.querySelectorAll("li");
+// const colors = ["red", "orange", "yellow", "green", "blue", "purple"];
+
+// allLis.forEach((li, i) => {
+//   const color = colors[i];
+//   li.style.color = color;
+// });
+// const h1 = document.querySelector("h1");
+
+// // style property only contains INLINE styles...
+// // Even though we gave the h1 a purple color, we still get:
+// h1.style.color; //""
+// // Same with any property we did not set inline:
+// h1.style.fontSize; //""
+
+// // We can use getComputedStyle to figure out the ACTUAL styles that are applying:
+// const compStyles = getComputedStyle(h1);
+// compStyles.color; //"rgb(128, 0, 128)"  (purple as an RGB color)
+// compStyles.fontSize; //"60px"
+// const todo = document.querySelector("#todos .todo");
+
+// // Setting styles one at a time is not ideal:
+// // todo.style.color = 'gray';
+// // todo.style.textDecoration = 'line-through';
+// // todo.style.opacity = '50%'
+
+// // We can use a class instead!
+// // In app.css I've defined a 'done' class that we can apply
+
+// // OPTION 1 - using setAttribute
+// //This adds the class 'done', but it overwrites any existing classes...
+// // todo.setAttribute('class', 'done');
+
+// // OPTION 2 - classList
+// // We can use the classList property and it's methods to add,remove, and toggle classes!
+// todo.classList.add("done");
+// //to remove it
+// todo.classList.remove("done");
+// //to toggle:
+// todo.classList.toggle("done"); //add
+// todo.classList.toggle("done"); //remove
+// todo.classList.toggle("done"); //add
+// todo.classList.toggle("done"); //remove
+// todo.classList.toggle("done"); //add
+// // Make a new empty img element:
+// const newImg = document.createElement("img");
+// // Add a src:
+// newImg.src =
+//   "https://images.unsplash.com/photo-1504006833117-8886a355efbf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80";
+// // Change its width:
+// newImg.style.width = "300px";
+
+// //Add it to the end of the body:
+// document.body.appendChild(newImg);
+
+// // Create a new anchor tag
+// const newLink = document.createElement("a");
+// // Set its innerText:
+// newLink.innerText = "Mr. Bubz Video! CLICK MEEE";
+// // Set its src:
+// newLink.href = "https://www.youtube.com/watch?v=QQNL83fhWJU";
+
+// // Select the first paragraph:
+// const firstP = document.querySelector("p");
+// // Add the link as a child of the paragraph:
+// firstP.appendChild(newLink);
+// const parentUL = document.querySelector("ul");
+// const newLI = document.createElement("li");
+// newLI.innerText = "I AM A NEW LIST ITEM!";
+
+// //prepend will add newLI as the FIRST child of parentUL
+// parentUL.prepend(newLI); //Doesn't work in IE!
+
+// //We can also insert something BEFORE another element, using insertBefore.
+// // First, select the element to insert before:
+// const targetLI = document.querySelectorAll("li.todo")[2]; //3rd li with class of 'todo'
+// // To insert our new LI before targetLI...
+// //parent.insertBefore(what to insert, where to insert)
+// parentUL.insertBefore(newLI, targetLI);
+// // USING removeChild()
+// //Select the element you want to remove;
+// const removeMe = document.querySelector(".special");
+// //We call removeChild() on the parent element and pass in the element we want to remove:
+// removeMe.parentElement.removeChild(removeMe);
+
+// // USING THE NEWER REMOVE() METHOD - NO INTERNET EXPLORER SUPPORT!
+// //Select the H1
+// const h1 = document.querySelector("h1");
+// h1.remove(); //REMOVE IT!
+
+// ***********************************
+// Two ways NOT to add event handlers
+// ***********************************
+
+// **********************************
+// Inline - take a look at index.html
+// **********************************
+
+// **********************************
+// Via JS - setting the onclick property
+// **********************************
+
+// Select an element:
+const btn = document.querySelector("#clicker");
+
+// Set the onclick property to a function:
+
+// You can use an existing function: (not that common)
+// btn.onclick = greet;
+
+// Or use an anonymous function (more common)
+btn.onclick = () => {
+  console.log("YOU CLICKED ME UGHHHH!!");
+};
+
+function greet() {
+  alert("HEY BUDDY!");
 }
-
-//We can use spread to make an actual array:
-const arr = [...specials];
-arr.pop(); //this works because it's now an array!
-
-// ****************
-// querySelector
-// ****************
-//To find the first li on the page:
-document.querySelector("li");
-
-//To find the first element with class of special:
-document.querySelector(".special");
-
-//To find the first element with id of main (there should only be one...)
-document.querySelector("#main");
-
-// To find the first li with the class of special, nested inside of a ul, nested inside a section:
-document.querySelector("section ul li.special");
-
-// ****************
-// querySelectorAll
-// ****************
-
-// To find ALL li's on the page:
-document.querySelectorAll("li");
-
-// To find ALL elements with the class of special on the page:
-document.querySelectorAll(".special");
-
-//
-// Select all LI's on the page:
-const allLis = document.querySelectorAll("li");
-
-// One option, a regular for loop:
-for (let i = 0; i < allLis.length; i++) {
-  allLis[i].innerText = "WE ARE THE CHAMPIONS!";
-}
-
-//Another option using for...of:
-for (let li of allLis) {
-  li.innerHTML = "WE ARE <b>THE CHAMPIONS</b>";
-}
-// Changing the color and background-color:
-const h1 = document.querySelector("h1");
-h1.style.color = "pink";
-h1.style.backgroundColor = "yellow"; //camel cased! (not background-color but backgroundColor)
-
-// Changing Multiple Elements:
-const allLis = document.querySelectorAll("li");
-const colors = ["red", "orange", "yellow", "green", "blue", "purple"];
-
-allLis.forEach((li, i) => {
-  const color = colors[i];
-  li.style.color = color;
-});
-const h1 = document.querySelector("h1");
-
-// style property only contains INLINE styles...
-// Even though we gave the h1 a purple color, we still get:
-h1.style.color; //""
-// Same with any property we did not set inline:
-h1.style.fontSize; //""
-
-// We can use getComputedStyle to figure out the ACTUAL styles that are applying:
-const compStyles = getComputedStyle(h1);
-compStyles.color; //"rgb(128, 0, 128)"  (purple as an RGB color)
-compStyles.fontSize; //"60px"
-const todo = document.querySelector("#todos .todo");
-
-// Setting styles one at a time is not ideal:
-// todo.style.color = 'gray';
-// todo.style.textDecoration = 'line-through';
-// todo.style.opacity = '50%'
-
-// We can use a class instead!
-// In app.css I've defined a 'done' class that we can apply
-
-// OPTION 1 - using setAttribute
-//This adds the class 'done', but it overwrites any existing classes...
-// todo.setAttribute('class', 'done');
-
-// OPTION 2 - classList
-// We can use the classList property and it's methods to add,remove, and toggle classes!
-todo.classList.add("done");
-//to remove it
-todo.classList.remove("done");
-//to toggle:
-todo.classList.toggle("done"); //add
-todo.classList.toggle("done"); //remove
-todo.classList.toggle("done"); //add
-todo.classList.toggle("done"); //remove
-todo.classList.toggle("done"); //add
-// Make a new empty img element:
-const newImg = document.createElement("img");
-// Add a src:
-newImg.src =
-  "https://images.unsplash.com/photo-1504006833117-8886a355efbf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80";
-// Change its width:
-newImg.style.width = "300px";
-
-//Add it to the end of the body:
-document.body.appendChild(newImg);
-
-// Create a new anchor tag
-const newLink = document.createElement("a");
-// Set its innerText:
-newLink.innerText = "Mr. Bubz Video! CLICK MEEE";
-// Set its src:
-newLink.href = "https://www.youtube.com/watch?v=QQNL83fhWJU";
-
-// Select the first paragraph:
-const firstP = document.querySelector("p");
-// Add the link as a child of the paragraph:
-firstP.appendChild(newLink);
-const parentUL = document.querySelector("ul");
-const newLI = document.createElement("li");
-newLI.innerText = "I AM A NEW LIST ITEM!";
-
-//prepend will add newLI as the FIRST child of parentUL
-parentUL.prepend(newLI); //Doesn't work in IE!
-
-//We can also insert something BEFORE another element, using insertBefore.
-// First, select the element to insert before:
-const targetLI = document.querySelectorAll("li.todo")[2]; //3rd li with class of 'todo'
-// To insert our new LI before targetLI...
-//parent.insertBefore(what to insert, where to insert)
-parentUL.insertBefore(newLI, targetLI);
-// USING removeChild()
-//Select the element you want to remove;
-const removeMe = document.querySelector(".special");
-//We call removeChild() on the parent element and pass in the element we want to remove:
-removeMe.parentElement.removeChild(removeMe);
-
-// USING THE NEWER REMOVE() METHOD - NO INTERNET EXPLORER SUPPORT!
-//Select the H1
-const h1 = document.querySelector("h1");
-h1.remove(); //REMOVE IT!
