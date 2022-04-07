5e9fcc84c1d62682b23aead8941b87f5c93cf070
Shokhmurod <shokhpjwstk@gmail.com>
2022-04-07 20:11:22 +0200

    practice with callbacks to understand in a better way

--- app.js
+++ app.js
@@ -1677,17 +1677,99 @@ const btn = document.querySelector("#clicker");
 //   console.log("STOP SCROLLING!!");
 // });
 
-const form = document.querySelector("#signup-form");
+// const form = document.querySelector("#signup-form");
 
-const creditCardInput = document.querySelector("#cc");
-const termsCheckbox = document.querySelector("#terms");
-const veggieSelect = document.querySelector("#veggie");
+// const creditCardInput = document.querySelector("#cc");
+// const termsCheckbox = document.querySelector("#terms");
+// const veggieSelect = document.querySelector("#veggie");
 
-form.addEventListener("submit", function (e) {
-  e.preventDefault(); //stops the request from being sent (prevents page reload)
-  console.log("cc", creditCardInput.value);
-  console.log("terms", termsCheckbox.checked);
-  console.log("veggieSelect", veggieSelect.value);
-  //send form data to db
-  //append something to page using form data
-});
+// form.addEventListener("submit", function (e) {
+//   e.preventDefault(); //stops the request from being sent (prevents page reload)
+//   console.log("cc", creditCardInput.value);
+//   console.log("terms", termsCheckbox.checked);
+//   console.log("veggieSelect", veggieSelect.value);
+//   //send form data to db
+//   //append something to page using form data
+// });
+
+const btn = document.querySelector("button");
+
+//This function moves an element "amount" number of pixels after a delay.
+//If the element will stay on screen, we move the element and call the onSuccess callback function
+//If the element will move off screen, we do not move the element and instead call the onFailure callback
+const moveX = (element, amount, delay, onSuccess, onFailure) => {
+  setTimeout(() => {
+    const bodyBoundary = document.body.clientWidth;
+    const elRight = element.getBoundingClientRect().right;
+    const currLeft = element.getBoundingClientRect().left;
+    if (elRight + amount > bodyBoundary) {
+      onFailure();
+    } else {
+      element.style.transform = `translateX(${currLeft + amount}px)`;
+      onSuccess();
+    }
+  }, delay);
+};
+
+// LOOK AT THIS UGLY MESS!
+moveX(
+  btn,
+  300,
+  1000,
+  () => {
+    //success callback
+    moveX(
+      btn,
+      300,
+      1000,
+      () => {
+        //success callback
+        moveX(
+          btn,
+          300,
+          1000,
+          () => {
+            //success callback
+            moveX(
+              btn,
+              300,
+              1000,
+              () => {
+                //success callback
+                moveX(
+                  btn,
+                  300,
+                  1000,
+                  () => {
+                    //success callback
+                    alert("YOU HAVE A WIDE SCREEN!");
+                  },
+                  () => {
+                    //failure callback
+                    alert("CANNOT MOVE FURTHER!");
+                  }
+                );
+              },
+              () => {
+                //failure callback
+                alert("CANNOT MOVE FURTHER!");
+              }
+            );
+          },
+          () => {
+            //failure callback
+            alert("CANNOT MOVE FURTHER!");
+          }
+        );
+      },
+      () => {
+        //failure callback
+        alert("CANNOT MOVE FURTHER!");
+      }
+    );
+  },
+  () => {
+    //failure callback
+    alert("CANNOT MOVE FURTHER!");
+  }
+);
